cdcffcbd368211e1aa48d966331f1bcf83246bb4
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-06-02 08:43:39 +0900

    add nightly source download ps1

---
+++ powershell/get-mroonga-nightly.ps1
@@ -0,0 +1,5 @@
+. ".\versions.ps1"
+
+$nightlyVer = "2014.06.01"
+
+Invoke-WebRequest -Uri http://packages.groonga.org/nightly/mariadb-${mariadbVer}-with-mroonga-${mroongaVer}-for-windows.${nightlyVer}.zip -OutFile source-nightly.zip
