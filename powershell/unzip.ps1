644a0da5f38db75e5e6e98e26c5e898e7e2807a7
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-06-01 21:56:13 +0900

    fix unzip.ps1

--- powershell/unzip.ps1
+++ powershell/unzip.ps1
@@ -1,8 +1,7 @@
+# need specify full path
 $file = "C:\Code\source.zip"
 $shell = New-Object -ComObject shell.application
 $zip = $shell.NameSpace($file)
 $dest =  $shell.NameSpace((Split-Path $file -Parent))
 
 $dest.CopyHere($zip.Items()) 
-
-move mariadb-*-for-windows source
