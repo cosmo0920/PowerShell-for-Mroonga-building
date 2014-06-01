3a9428ddfb740f4440fb2a970646360d69333fec
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-06-01 23:00:27 +0900

    add prepare powershell script

---
+++ powershell/prepare-building-mroonga.ps1
@@ -0,0 +1,8 @@
+$workDir = "work"
+$sourceDir = "source"
+if (Test-Path -path $workDir) {
+  Remove-Item $workDir -Recurse
+}
+New-Item $workDir -itemType directory
+Move-Item mariadb-*-for-windows $workDir/$sourceDir
+Copy-Item batfiles/build-*.bat $workDir
