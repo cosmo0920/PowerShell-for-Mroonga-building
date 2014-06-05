c2ebfe0b860c5f08c6b8997f0f49e46646c05a52
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-06-06 01:48:31 +0900

    add echo to output build process

--- powershell/build-vc2013.ps1
+++ powershell/build-vc2013.ps1
@@ -20,16 +20,22 @@ if($result -eq 1) {
 }
 
 # prepare building Mroonga
+echo "extract Mroonga $mroongaVer zip"
 .\unzip.ps1
+echo "prepare Mroonga $mroongaVer building"
 .\prepare-building-mroonga.ps1
 
 # build MariaDB bundled Mroonga
+echo "building Mroonga $mroongaVer with MariaDB $mariadbVer ..."
 cd $workDir
 cmd /c "build-vc2013-zip-32.bat"
 cmd /c "build-vc2013-zip-64.bat"
 cd $originDir
 
 # packaging Mroonga
+echo "unpack Mroonga $mroongaVer packed zip"
 .\package-unzip.ps1
+echo "install Mroonga $mroongaVer to MariaDB"
 .\install-mroonga.ps1
+echo "packing Mroonga $mroongaVer"
 .\create-mrnzip.ps1
