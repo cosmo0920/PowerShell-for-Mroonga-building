8899ef3a933347da00bf7643c0d430912f8ce4c0
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-06-06 20:25:53 +0900

    use Get-Date Cmdlet to download mroonga nightly

--- powershell/get-mroonga-nightly.ps1
+++ powershell/get-mroonga-nightly.ps1
@@ -1,5 +1,6 @@
 . ".\versions.ps1"
 
-$nightlyVer = "2014.06.04"
+$today = Get-Date -Format "yyyy.MM.dd"
+$nightlyVer = "$today"
 
 Invoke-WebRequest -Uri http://packages.groonga.org/nightly/mariadb-${mariadbVer}-with-mroonga-${mroongaVer}-for-windows.${nightlyVer}.zip -OutFile source-nightly.zip
