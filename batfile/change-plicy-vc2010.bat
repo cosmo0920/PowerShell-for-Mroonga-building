01421029ca224db9bcbade9cca2339ba93c2ec96
hhatake <hhatake@aiming-inc.com>
2014-06-06 15:49:23 +0900

    add change policy batfiles

---
+++ batfile/change-plicy-vc2010.bat
@@ -0,0 +1,3 @@
+powershell Set-ExecutionPolicy RemoteSigned
+powershell ..\powershell\build-vc2010.ps1
+powershell Set-ExecutionPolicy Restricted
---
+++ batfile/change-plicy-vc2013.bat
@@ -0,0 +1,3 @@
+powershell Set-ExecutionPolicy RemoteSigned
+powershell ..\powershell\build-vc2013.ps1
+powershell Set-ExecutionPolicy Restricted
