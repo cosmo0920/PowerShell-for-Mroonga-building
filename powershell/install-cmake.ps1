d82e23f450c5d2330593d9504002d494e1346217
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-07-05 00:03:42 +0900

    add old cmake install ps1

---
+++ powershell/install-cmake.ps1
@@ -0,0 +1,12 @@
+$cmakeVersion = "2.8.11.2"
+
+Invoke-WebRequest -Uri http://www.cmake.org/files/v2.8/cmake-${cmakeVersion}-win32-x86.zip -OutFile cmake.zip
+
+$pathToZip = "$pwd\cmake.zip"
+$targetDir = "$pwd\cmake"
+#Load the assembly
+[System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem") | Out-Null
+[System.IO.Compression.ZipFile]::ExtractToDirectory($pathToZip, $targetDir)
+
+# set Path
+$Env:Path = "$targetDir\cmake-${cmakeVersion}\win32-x86;" + $Env:Path
