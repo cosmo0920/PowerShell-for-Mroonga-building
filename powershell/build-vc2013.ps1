2558a5e4b692b2a1c6694098be2832aa99179c75
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-07-05 00:47:14 +0900

    catch `Exception calling "PromptForChoice" with "4" argument(s)`

--- powershell/build-vc2013.ps1
+++ powershell/build-vc2013.ps1
@@ -5,18 +5,24 @@ $no = New-Object System.Management.Automation.Host.ChoiceDescription "&No",""
 $choices = [System.Management.Automation.Host.ChoiceDescription[]]($yes,$no)
 $caption = "== Mroonga source download =="
 $message = "Do you want to use nightly package?"
-$result = $Host.UI.PromptForChoice($caption,$message,$choices,0)
-# download Mroonga source
-if($result -eq 0) {
+try {
+  $result = $Host.UI.PromptForChoice($caption,$message,$choices,0)
+  # download Mroonga source
+  if($result -eq 0) {
+    Write-Host "Use Mroonga $mroongaVer nightly source"
+    .\get-mroonga-nightly.ps1
+    $zipname = "source-nightly.zip"
+  }
+
+  if($result -eq 1) {
+    Write-Host "Use Mroonga $mroongaVer source"
+    .\get-mroonga.ps1
+    $zipname = "source.zip"
+  }
+} catch {
   Write-Host "Use Mroonga $mroongaVer nightly source"
   .\get-mroonga-nightly.ps1
   $zipname = "source-nightly.zip"
-}
-
-if($result -eq 1) {
-  Write-Host "Use Mroonga $mroongaVer source"
-  .\get-mroonga.ps1
-  $zipname = "source.zip"
 }
 
 # prepare building Mroonga
--- powershell/install-cmake.ps1
+++ powershell/install-cmake.ps1
@@ -3,7 +3,6 @@ $cmakeVersion = "2.8.11.2"
 Invoke-WebRequest -Uri http://www.cmake.org/files/v2.8/cmake-${cmakeVersion}-win32-x86.zip -OutFile cmake.zip
 
 $pathToZip = "$pwd\cmake.zip"
-$targetDir = "$pwd\cmake"
 #Load the assembly
 [System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem") | Out-Null
 [System.IO.Compression.ZipFile]::ExtractToDirectory($pathToZip, $pwd)
