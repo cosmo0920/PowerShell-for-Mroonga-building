671debaba1c7cbf540fbe5ea692bc6dbe4526be8
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-08-29 11:19:44 +0900

    install WiX39 to use Appveyor

---
+++ powershell/install-wix39zip.ps1
@@ -0,0 +1,10 @@
+$wixArchiveName = "wix39-binaries.zip"
+Invoke-WebRequest -Uri http://wixtoolset.org/downloads/v3.9.818.0/${wixArchiveName} -OutFile ${wixArchiveName}
+
+$pathToZip = "$pwd\${wixArchiveName}"
+#Load the assembly
+[System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem") | Out-Null
+[System.IO.Compression.ZipFile]::ExtractToDirectory($pathToZip, $pwd)
+
+# set Path
+$Env:Path = "$pwd\${wixArchiveName};" + $Env:Path
