435cc0b1f1bbed40fb0fa442d1bbae920a19a761
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-08-29 12:07:16 +0900

    extract wix to $pwd/$wixArchiveName

--- powershell/install-wix39zip.ps1
+++ powershell/install-wix39zip.ps1
@@ -4,7 +4,7 @@ Invoke-WebRequest -Uri http://wixtoolset.org/downloads/v3.9.818.0/${wixArchiveNa
 $pathToZip = "$pwd\${wixArchiveName}"
 #Load the assembly
 [System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem") | Out-Null
-[System.IO.Compression.ZipFile]::ExtractToDirectory($pathToZip, $pwd)
+[System.IO.Compression.ZipFile]::ExtractToDirectory($pathToZip, "$pwd\${wixArchiveName}")
 
 # set Path
 $Env:Path = "$pwd\${wixArchiveName};" + $Env:Path
