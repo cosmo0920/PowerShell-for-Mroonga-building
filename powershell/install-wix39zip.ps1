89dfb797ccf3334ca3568d361470954975c56eca
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-08-29 12:20:28 +0900

    specify WinInstallDir

--- powershell/install-wix39zip.ps1
+++ powershell/install-wix39zip.ps1
@@ -1,10 +1,12 @@
 $wixArchiveName = "wix39-binaries.zip"
+$winInstallDir = "wix39-binaries"
+
 Invoke-WebRequest -Uri http://wixtoolset.org/downloads/v3.9.818.0/${wixArchiveName} -OutFile ${wixArchiveName}
 
 $pathToZip = "$pwd\${wixArchiveName}"
 #Load the assembly
 [System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem") | Out-Null
-[System.IO.Compression.ZipFile]::ExtractToDirectory($pathToZip, "$pwd\${wixArchiveName}")
+[System.IO.Compression.ZipFile]::ExtractToDirectory($pathToZip, "$pwd\${wixInstallDir}")
 
 # set Path
 $Env:Path = "$pwd\${wixArchiveName};" + $Env:Path
