4cf077809837dbec39f54732a678e35cf1acdbff
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-11-28 00:27:23 +0900

    make to be able to sepcify $nightlyVer in build script
    e.g.
    ```powershell
    $vcVer = "vc2013"
    $nightlyVer = "2014.11.27" # <- like this
    .\build-vc-core.ps1
    ```

--- powershell/get-mroonga-nightly.ps1
+++ powershell/get-mroonga-nightly.ps1
@@ -1,6 +1,8 @@
 . ".\versions.ps1"
 
-$today = Get-Date -Format "yyyy.MM.dd"
-$nightlyVer = "$today"
+if ($nightlyVer -eq $null) {
+  $today = Get-Date -Format "yyyy.MM.dd"
+  $nightlyVer = "$today"
+}
 
 Invoke-WebRequest -Uri http://packages.groonga.org/nightly/mariadb-${mariadbVer}-with-mroonga-${mroongaVer}-for-windows.${nightlyVer}.zip -OutFile source-nightly.zip
