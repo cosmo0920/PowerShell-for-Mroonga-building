39fcb59bc13f8bcac6fee716fa25baa25ded0e04
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-12-02 22:49:08 +0900

    add package downloading skip feature
    This feature how to use like this(in build-vc-(2010|2013).ps1):
    
    ```powershell
    $downloadRequest = $FALSE
    ```

--- powershell/build-vc-core.ps1
+++ powershell/build-vc-core.ps1
@@ -1,28 +1,32 @@
 . ".\versions.ps1"
 
-$yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes",""
-$no = New-Object System.Management.Automation.Host.ChoiceDescription "&No",""
-$choices = [System.Management.Automation.Host.ChoiceDescription[]]($yes,$no)
-$caption = "== Mroonga source download =="
-$message = "Do you want to use nightly package?"
-try {
-  $result = $Host.UI.PromptForChoice($caption,$message,$choices,0)
-  # download Mroonga source
-  if($result -eq 0) {
+if($downloadRequestSkip -eq $null) {
+  Write-Host "Package downloading skipped."
+} else {
+  $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes",""
+  $no = New-Object System.Management.Automation.Host.ChoiceDescription "&No",""
+  $choices = [System.Management.Automation.Host.ChoiceDescription[]]($yes,$no)
+  $caption = "== Mroonga source download =="
+  $message = "Do you want to use nightly package?"
+  try {
+    $result = $Host.UI.PromptForChoice($caption,$message,$choices,0)
+    # download Mroonga source
+    if($result -eq 0) {
+      Write-Host "Use Mroonga $mroongaVer nightly source"
+      .\get-mroonga-nightly.ps1
+      $zipname = "source-nightly.zip"
+    }
+
+    if($result -eq 1) {
+      Write-Host "Use Mroonga $mroongaVer source"
+      .\get-mroonga.ps1
+      $zipname = "source.zip"
+    }
+  } catch [NotImplementedException] {
     Write-Host "Use Mroonga $mroongaVer nightly source"
     .\get-mroonga-nightly.ps1
     $zipname = "source-nightly.zip"
   }
-
-  if($result -eq 1) {
-    Write-Host "Use Mroonga $mroongaVer source"
-    .\get-mroonga.ps1
-    $zipname = "source.zip"
-  }
-} catch [NotImplementedException] {
-  Write-Host "Use Mroonga $mroongaVer nightly source"
-  .\get-mroonga-nightly.ps1
-  $zipname = "source-nightly.zip"
 }
 
 # prepare building Mroonga
