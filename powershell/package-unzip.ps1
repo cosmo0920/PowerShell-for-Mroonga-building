1b2f8d643a0567d8872899e24438dceec293cd2a
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-12-02 23:05:46 +0900

    add null check for unzip $file

--- powershell/package-unzip.ps1
+++ powershell/package-unzip.ps1
@@ -3,6 +3,11 @@
 
 function unzipMrnPackage($workDir, $mariaDBVer, $arch) {
   $file = "$workDir\mariadb-$mariadbVer-$arch.zip"
+
+  if ($file -eq $null) {
+    Write-Host "Could not find valid built package."
+    exit 1
+  }
   #Load the assembly
   [System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem") | Out-Null
   [System.IO.Compression.ZipFile]::ExtractToDirectory($file, $pwd)
