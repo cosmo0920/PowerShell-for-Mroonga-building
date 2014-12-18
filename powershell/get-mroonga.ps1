ce353a06f30b34bdb17c04ea1d9a91452c95606d
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-12-18 22:59:55 +0900

    remove needless space

--- powershell/get-mroonga.ps1
+++ powershell/get-mroonga.ps1
@@ -3,6 +3,6 @@
 try {
   Invoke-WebRequest -Uri http://packages.groonga.org/source/mroonga/mariadb-${mariadbVer}-with-mroonga-${mroongaVer}-for-windows.zip -OutFile source.zip
 } catch [System.Net.WebException] {
-  Write-Host "Not Found package. "
+  Write-Host "Not Found package."
   Exit 1
 }
