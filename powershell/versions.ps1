0ee2d1c379dd19bdde89d8167376133d2b30a9ef
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-01-29 20:26:12 +0900

    Bump up using package versions

--- powershell/versions.ps1
+++ powershell/versions.ps1
@@ -1,7 +1,7 @@
 $originDir = $(Get-Item ".").FullName
 $workDir = "$originDir\work"
-$mariadbVer = "10.0.15"
-$mroongaVer = "4.09"
+$mariadbVer = "10.0.16"
+$mroongaVer = "4.10"
 $useNightly = $null
 #$useReleasePackage = $null
 $downloadRequestSkip = $null
