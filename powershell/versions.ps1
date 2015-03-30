3bda21b042ef1ea7a43257e11cee52328171c5bb
Hiroshi Hatake <hatake@clear-code.com>
2015-03-30 11:09:13 +0900

    bump up MariaDB and Mroonga versions

--- powershell/versions.ps1
+++ powershell/versions.ps1
@@ -1,7 +1,7 @@
 $originDir = $(Get-Item ".").FullName
 $workDir = "$originDir\work"
-$mariadbVer = "10.0.16"
-$mroongaVer = "5.00"
+$mariadbVer = "10.0.17"
+$mroongaVer = "5.01"
 $useNightly = $null
 #$useReleasePackage = $null
 $downloadRequestSkip = $null
