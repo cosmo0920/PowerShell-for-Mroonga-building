67b09e4ad803a9a3e7d718e5c8997024c0fcc730
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2015-05-16 00:03:18 +0900

    Use MariaDB 10.0.18

--- powershell/versions.ps1
+++ powershell/versions.ps1
@@ -1,6 +1,6 @@
 $originDir = $(Get-Item ".").FullName
 $workDir = "$originDir\work"
-$mariadbVer = "10.0.17"
+$mariadbVer = "10.0.18"
 $mroongaVer = "5.02"
 $useNightly = $null
 #$useReleasePackage = $null
