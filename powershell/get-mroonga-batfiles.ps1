cf1d581baed18a4784fe4e22f47cd9b2643d35d0
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2015-05-16 22:29:03 +0900

    Start to use VC2015

--- powershell/get-mroonga-batfiles.ps1
+++ powershell/get-mroonga-batfiles.ps1
@@ -4,16 +4,16 @@ $batfileDir = "batfiles"
 if (!(Test-Path -path $batfileDir)) {
   New-Item $batfileDir -itemType directory
 }
-$batfiles = "build-vc2010.bat",
-"build-vc2010-zip-32.bat",
-"build-vc2010-zip-64.bat",
-"build-vc2010-msi-32.bat",
-"build-vc2010-msi-64.bat",
-"build-vc2013.bat",
+$batfiles =  "build-vc2013.bat",
 "build-vc2013-zip-32.bat",
 "build-vc2013-zip-64.bat",
 "build-vc2013-msi-32.bat",
-"build-vc2013-msi-64.bat"
+"build-vc2013-msi-64.bat",
+"build-vc2015.bat",
+"build-vc2015-zip-32.bat",
+"build-vc2015-zip-64.bat",
+"build-vc2015-msi-32.bat",
+"build-vc2015-msi-64.bat"
 
 foreach ($file in $batfiles)
 {
