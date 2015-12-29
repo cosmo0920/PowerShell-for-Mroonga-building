8c83508794c81c5428cb4c5fff05c5b66249cd2a
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2015-12-29 12:48:00 +0900

    Bump up MariaDB version to 10.1.10

--- powershell/versions.ps1
+++ powershell/versions.ps1
@@ -1,6 +1,6 @@
 $originDir = $(Get-Item ".").FullName
 $workDir = "$originDir\work"
-$mariadbVer = "10.1.8"
+$mariadbVer = "10.1.10"
 $mroongaVer = "5.11"
 $useNightly = $null
 #$useReleasePackage = $null
