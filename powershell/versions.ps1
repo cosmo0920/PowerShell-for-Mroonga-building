90d6a7f53b5c3238951f083daae0bdbe49556524
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2016-03-30 00:42:09 +0900

    Mroonga 6.02 nightly

--- powershell/versions.ps1
+++ powershell/versions.ps1
@@ -1,7 +1,7 @@
 $originDir = $(Get-Item ".").FullName
 $workDir = "$originDir\work"
 $mariadbVer = "10.1.13"
-$mroongaVer = "6.01"
+$mroongaVer = "6.02"
 $useNightly = $null
 #$useReleasePackage = $null
 $downloadRequestSkip = $null
