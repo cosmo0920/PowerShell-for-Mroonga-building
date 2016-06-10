eef39edc10dda317d0bae5b55ad4bc8e46793f33
Hiroshi Hatake <hatake@clear-code.com>
2016-06-10 12:40:42 +0900

    Bump up Mroonga version to 6.05

--- powershell/versions.ps1
+++ powershell/versions.ps1
@@ -1,7 +1,7 @@
 $originDir = $(Get-Item ".").FullName
 $workDir = "$originDir\work"
 $mariadbVer = "10.1.14"
-$mroongaVer = "6.03"
+$mroongaVer = "6.05"
 $useNightly = $null
 #$useReleasePackage = $null
 $downloadRequestSkip = $null
