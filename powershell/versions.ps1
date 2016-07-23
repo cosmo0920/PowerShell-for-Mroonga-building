8bbbf5ab356dddd225cdf2f9e8398b0b6f531f03
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2016-07-23 14:21:09 +0900

    Update MariaDB version to 10.1.16

--- powershell/versions.ps1
+++ powershell/versions.ps1
@@ -1,6 +1,6 @@
 $originDir = $(Get-Item ".").FullName
 $workDir = "$originDir\work"
-$mariadbVer = "10.1.14"
+$mariadbVer = "10.1.16"
 $mroongaVer = "6.07"
 $useNightly = $null
 #$useReleasePackage = $null
