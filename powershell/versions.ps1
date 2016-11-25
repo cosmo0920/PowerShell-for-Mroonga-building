5d6d308062338f0e282b8e734e5bb18a0eecd14b
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2016-11-26 01:03:12 +0900

    Bump up MariaDB version to 10.1.19

--- powershell/versions.ps1
+++ powershell/versions.ps1
@@ -1,6 +1,6 @@
 $originDir = $(Get-Item ".").FullName
 $workDir = "$originDir\work"
-$mariadbVer = "10.1.18"
+$mariadbVer = "10.1.19"
 $mroongaVer = "6.11"
 $useNightly = $null
 #$useReleasePackage = $null
