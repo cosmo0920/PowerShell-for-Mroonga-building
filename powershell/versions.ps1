1b554273e3873abeaf14da705ce826ba2dd83257
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2017-06-29 13:46:13 +0900

    Bump up MariaDB version to 10.1.24

--- powershell/versions.ps1
+++ powershell/versions.ps1
@@ -1,6 +1,6 @@
 $originDir = $(Get-Item ".").FullName
 $workDir = "$originDir\work"
-$mariadbVer = "10.1.23"
+$mariadbVer = "10.1.24"
 $mroongaVer = "7.04"
 $useNightly = $null
 #$useReleasePackage = $null
