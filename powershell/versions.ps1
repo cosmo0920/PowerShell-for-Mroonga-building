ef9ab85b201875d2560567f78b83e58d480d2220
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2017-10-26 10:25:01 +0900

    Bump up Mroonga version to 7.08

--- powershell/versions.ps1
+++ powershell/versions.ps1
@@ -1,7 +1,7 @@
 $originDir = $(Get-Item ".").FullName
 $workDir = "$originDir\work"
 $mariadbVer = "10.1.28"
-$mroongaVer = "7.07"
+$mroongaVer = "7.08"
 $useNightly = $null
 #$useReleasePackage = $null
 $downloadRequestSkip = $null
