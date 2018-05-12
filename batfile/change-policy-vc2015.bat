b66d7db84ccc23c26ae78b8e09af1bf76016e35d
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2018-05-12 15:55:07 +0900

    Remove Visual Studio 2015 related files

--- batfile/change-policy-vc2015.bat
+++
@@ -1,3 +0,0 @@
-powershell Set-ExecutionPolicy RemoteSigned
-powershell ..\powershell\build-vc2015.ps1
-powershell Set-ExecutionPolicy Restricted
--- powershell/build-vc2015.ps1
+++
@@ -1,4 +0,0 @@
-$vcVer = "vc2015"
-$applyPatch = $TRUE
-# $useReleasePackage = $TRUE
-.\build-vc-core.ps1
